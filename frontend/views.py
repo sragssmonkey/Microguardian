07450e06ddd25c48fa52e613c44ca5fe75e03edc
Sragvi Chavan <sragvichavan3954@gmail.com>
2025-09-28 00:12:20 +0530

    Added 3D model

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, "frontend/index.html")
+def model(request):
+    return render(request, "frontend/model.html")
--- microguardian/settings.py
+++ microguardian/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
